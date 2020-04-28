c2fa60096d4faac9e0fd90510d432c085dd4d6a2
hideraldobarbosa <hideraldo.barbosa@gmail.com>
2020-04-27 23:17:10 -0300

    Objeto List - Basico

---
+++ todoListBasico/src/app/demos/todo-list/components/tasks-iniciadas/tasks-iniciadas.component.ts
@@ -0,0 +1,28 @@
+import { Store } from './../../todo.store';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { TasksService } from '../../todo.service';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'tasks-iniciadas',
+  templateUrl: './tasks-iniciadas.component.html'
+})
+export class TasksIniciadasComponent implements OnInit {
+
+  iniciados$: Observable<any[]>;
+
+  constructor(private tasksService: TasksService, private store : Store) {}
+
+  ngOnInit() {
+    this.iniciados$ = this.store.getTodoList()
+    .pipe(
+      map(todolist => todolist.filter(task => task.iniciado && !task.finalizado))
+    ); 
+  }
+  onToggle(event){
+    this.tasksService.toggle(event);
+  }
+
+}
